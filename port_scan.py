a0af574710eb72abb79ba7a1086bac3a3da0a45c
SheldonZheng <zhenghangtxdyr@gmail.com>
2016-09-04 10:29:18 +0800

    单线程测试通过，完成

--- port_scan.py
+++ port_scan.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import sys
+from socket import *
 
 # port_scan.py <host> <start_port>-<end_port>
 
@@ -13,3 +14,20 @@ end_port = int(portstrs[1])
 
 print (start_port)
 print (end_port)
+
+target_ip = gethostbyname(host)
+
+opened_ports = []
+
+for port in range(start_port,end_port + 1):
+    sock = socket(AF_INET, SOCK_STREAM)
+    sock.settimeout(10)
+    result = sock.connect_ex((target_ip,port))
+    if result == 0:
+        opened_ports.append(port)
+
+print('Opened ports:')
+
+for i in opened_ports:
+    print(i)
+
